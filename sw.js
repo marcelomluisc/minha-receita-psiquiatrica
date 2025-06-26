47975e1281fc5e772b271c2d009aa9d05662f184
marcelomluisc <marcelomluisc@gmail.com>
2025-06-26 10:07:35 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
-self.addEventListener('fetch', function(event) {});
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open("app-cache").then(function(cache) {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/sw.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
